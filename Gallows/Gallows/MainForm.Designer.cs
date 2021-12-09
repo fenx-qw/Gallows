7f6375483ffc2cfb6fe369c7167f7148147797e8
fenxqw <55843532+fenx-qw@users.noreply.github.com>
2021-12-09 20:14:35 +0300

    Created project and main forms

---
+++ Gallows/Gallows/MainForm.Designer.cs
@@ -0,0 +1,458 @@
+﻿
+namespace Gallows
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.char1 = new System.Windows.Forms.Button();
+            this.char2 = new System.Windows.Forms.Button();
+            this.char3 = new System.Windows.Forms.Button();
+            this.char4 = new System.Windows.Forms.Button();
+            this.char5 = new System.Windows.Forms.Button();
+            this.char6 = new System.Windows.Forms.Button();
+            this.char7 = new System.Windows.Forms.Button();
+            this.char8 = new System.Windows.Forms.Button();
+            this.char9 = new System.Windows.Forms.Button();
+            this.char10 = new System.Windows.Forms.Button();
+            this.char11 = new System.Windows.Forms.Button();
+            this.char12 = new System.Windows.Forms.Button();
+            this.char13 = new System.Windows.Forms.Button();
+            this.char14 = new System.Windows.Forms.Button();
+            this.char15 = new System.Windows.Forms.Button();
+            this.char16 = new System.Windows.Forms.Button();
+            this.char17 = new System.Windows.Forms.Button();
+            this.char18 = new System.Windows.Forms.Button();
+            this.char19 = new System.Windows.Forms.Button();
+            this.char20 = new System.Windows.Forms.Button();
+            this.char21 = new System.Windows.Forms.Button();
+            this.char22 = new System.Windows.Forms.Button();
+            this.char23 = new System.Windows.Forms.Button();
+            this.char24 = new System.Windows.Forms.Button();
+            this.char25 = new System.Windows.Forms.Button();
+            this.char26 = new System.Windows.Forms.Button();
+            this.char27 = new System.Windows.Forms.Button();
+            this.char28 = new System.Windows.Forms.Button();
+            this.char29 = new System.Windows.Forms.Button();
+            this.char30 = new System.Windows.Forms.Button();
+            this.char31 = new System.Windows.Forms.Button();
+            this.char32 = new System.Windows.Forms.Button();
+            this.char33 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(64, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(327, 367);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // char1
+            // 
+            this.char1.Location = new System.Drawing.Point(411, 105);
+            this.char1.Name = "char1";
+            this.char1.Size = new System.Drawing.Size(40, 40);
+            this.char1.TabIndex = 1;
+            this.char1.Text = "А";
+            this.char1.UseVisualStyleBackColor = true;
+            // 
+            // char2
+            // 
+            this.char2.Location = new System.Drawing.Point(460, 105);
+            this.char2.Name = "char2";
+            this.char2.Size = new System.Drawing.Size(40, 40);
+            this.char2.TabIndex = 1;
+            this.char2.Text = "Б";
+            this.char2.UseVisualStyleBackColor = true;
+            // 
+            // char3
+            // 
+            this.char3.Location = new System.Drawing.Point(509, 105);
+            this.char3.Name = "char3";
+            this.char3.Size = new System.Drawing.Size(40, 40);
+            this.char3.TabIndex = 1;
+            this.char3.Text = "В";
+            this.char3.UseVisualStyleBackColor = true;
+            // 
+            // char4
+            // 
+            this.char4.Location = new System.Drawing.Point(558, 105);
+            this.char4.Name = "char4";
+            this.char4.Size = new System.Drawing.Size(40, 40);
+            this.char4.TabIndex = 1;
+            this.char4.Text = "Г";
+            this.char4.UseVisualStyleBackColor = true;
+            // 
+            // char5
+            // 
+            this.char5.Location = new System.Drawing.Point(607, 105);
+            this.char5.Name = "char5";
+            this.char5.Size = new System.Drawing.Size(40, 40);
+            this.char5.TabIndex = 1;
+            this.char5.Text = "Д";
+            this.char5.UseVisualStyleBackColor = true;
+            // 
+            // char6
+            // 
+            this.char6.Location = new System.Drawing.Point(656, 105);
+            this.char6.Name = "char6";
+            this.char6.Size = new System.Drawing.Size(40, 40);
+            this.char6.TabIndex = 1;
+            this.char6.Text = "Е";
+            this.char6.UseVisualStyleBackColor = true;
+            // 
+            // char7
+            // 
+            this.char7.Location = new System.Drawing.Point(705, 105);
+            this.char7.Name = "char7";
+            this.char7.Size = new System.Drawing.Size(40, 40);
+            this.char7.TabIndex = 1;
+            this.char7.Text = "Ё";
+            this.char7.UseVisualStyleBackColor = true;
+            // 
+            // char8
+            // 
+            this.char8.Location = new System.Drawing.Point(754, 105);
+            this.char8.Name = "char8";
+            this.char8.Size = new System.Drawing.Size(40, 40);
+            this.char8.TabIndex = 1;
+            this.char8.Text = "Ж";
+            this.char8.UseVisualStyleBackColor = true;
+            // 
+            // char9
+            // 
+            this.char9.Location = new System.Drawing.Point(411, 163);
+            this.char9.Name = "char9";
+            this.char9.Size = new System.Drawing.Size(40, 40);
+            this.char9.TabIndex = 1;
+            this.char9.Text = "З";
+            this.char9.UseVisualStyleBackColor = true;
+            // 
+            // char10
+            // 
+            this.char10.Location = new System.Drawing.Point(460, 163);
+            this.char10.Name = "char10";
+            this.char10.Size = new System.Drawing.Size(40, 40);
+            this.char10.TabIndex = 1;
+            this.char10.Text = "И";
+            this.char10.UseVisualStyleBackColor = true;
+            // 
+            // char11
+            // 
+            this.char11.Location = new System.Drawing.Point(509, 163);
+            this.char11.Name = "char11";
+            this.char11.Size = new System.Drawing.Size(40, 40);
+            this.char11.TabIndex = 1;
+            this.char11.Text = "Й";
+            this.char11.UseVisualStyleBackColor = true;
+            // 
+            // char12
+            // 
+            this.char12.Location = new System.Drawing.Point(558, 163);
+            this.char12.Name = "char12";
+            this.char12.Size = new System.Drawing.Size(40, 40);
+            this.char12.TabIndex = 1;
+            this.char12.Text = "К";
+            this.char12.UseVisualStyleBackColor = true;
+            // 
+            // char13
+            // 
+            this.char13.Location = new System.Drawing.Point(607, 163);
+            this.char13.Name = "char13";
+            this.char13.Size = new System.Drawing.Size(40, 40);
+            this.char13.TabIndex = 1;
+            this.char13.Text = "Л";
+            this.char13.UseVisualStyleBackColor = true;
+            // 
+            // char14
+            // 
+            this.char14.Location = new System.Drawing.Point(656, 163);
+            this.char14.Name = "char14";
+            this.char14.Size = new System.Drawing.Size(40, 40);
+            this.char14.TabIndex = 1;
+            this.char14.Text = "М";
+            this.char14.UseVisualStyleBackColor = true;
+            // 
+            // char15
+            // 
+            this.char15.Location = new System.Drawing.Point(705, 163);
+            this.char15.Name = "char15";
+            this.char15.Size = new System.Drawing.Size(40, 40);
+            this.char15.TabIndex = 1;
+            this.char15.Text = "Н";
+            this.char15.UseVisualStyleBackColor = true;
+            // 
+            // char16
+            // 
+            this.char16.Location = new System.Drawing.Point(754, 163);
+            this.char16.Name = "char16";
+            this.char16.Size = new System.Drawing.Size(40, 40);
+            this.char16.TabIndex = 1;
+            this.char16.Text = "О";
+            this.char16.UseVisualStyleBackColor = true;
+            // 
+            // char17
+            // 
+            this.char17.Location = new System.Drawing.Point(411, 221);
+            this.char17.Name = "char17";
+            this.char17.Size = new System.Drawing.Size(40, 40);
+            this.char17.TabIndex = 1;
+            this.char17.Text = "П";
+            this.char17.UseVisualStyleBackColor = true;
+            // 
+            // char18
+            // 
+            this.char18.Location = new System.Drawing.Point(460, 221);
+            this.char18.Name = "char18";
+            this.char18.Size = new System.Drawing.Size(40, 40);
+            this.char18.TabIndex = 1;
+            this.char18.Text = "Р";
+            this.char18.UseVisualStyleBackColor = true;
+            // 
+            // char19
+            // 
+            this.char19.Location = new System.Drawing.Point(509, 221);
+            this.char19.Name = "char19";
+            this.char19.Size = new System.Drawing.Size(40, 40);
+            this.char19.TabIndex = 1;
+            this.char19.Text = "С";
+            this.char19.UseVisualStyleBackColor = true;
+            // 
+            // char20
+            // 
+            this.char20.Location = new System.Drawing.Point(558, 221);
+            this.char20.Name = "char20";
+            this.char20.Size = new System.Drawing.Size(40, 40);
+            this.char20.TabIndex = 1;
+            this.char20.Text = "Т";
+            this.char20.UseVisualStyleBackColor = true;
+            // 
+            // char21
+            // 
+            this.char21.Location = new System.Drawing.Point(607, 221);
+            this.char21.Name = "char21";
+            this.char21.Size = new System.Drawing.Size(40, 40);
+            this.char21.TabIndex = 1;
+            this.char21.Text = "У";
+            this.char21.UseVisualStyleBackColor = true;
+            // 
+            // char22
+            // 
+            this.char22.Location = new System.Drawing.Point(656, 221);
+            this.char22.Name = "char22";
+            this.char22.Size = new System.Drawing.Size(40, 40);
+            this.char22.TabIndex = 1;
+            this.char22.Text = "Ф";
+            this.char22.UseVisualStyleBackColor = true;
+            // 
+            // char23
+            // 
+            this.char23.Location = new System.Drawing.Point(705, 221);
+            this.char23.Name = "char23";
+            this.char23.Size = new System.Drawing.Size(40, 40);
+            this.char23.TabIndex = 1;
+            this.char23.Text = "Х";
+            this.char23.UseVisualStyleBackColor = true;
+            // 
+            // char24
+            // 
+            this.char24.Location = new System.Drawing.Point(754, 221);
+            this.char24.Name = "char24";
+            this.char24.Size = new System.Drawing.Size(40, 40);
+            this.char24.TabIndex = 1;
+            this.char24.Text = "Ц";
+            this.char24.UseVisualStyleBackColor = true;
+            // 
+            // char25
+            // 
+            this.char25.Location = new System.Drawing.Point(415, 280);
+            this.char25.Name = "char25";
+            this.char25.Size = new System.Drawing.Size(40, 40);
+            this.char25.TabIndex = 1;
+            this.char25.Text = "Ч";
+            this.char25.UseVisualStyleBackColor = true;
+            // 
+            // char26
+            // 
+            this.char26.Location = new System.Drawing.Point(457, 280);
+            this.char26.Name = "char26";
+            this.char26.Size = new System.Drawing.Size(40, 40);
+            this.char26.TabIndex = 1;
+            this.char26.Text = "Ш";
+            this.char26.UseVisualStyleBackColor = true;
+            // 
+            // char27
+            // 
+            this.char27.Location = new System.Drawing.Point(499, 280);
+            this.char27.Name = "char27";
+            this.char27.Size = new System.Drawing.Size(40, 40);
+            this.char27.TabIndex = 1;
+            this.char27.Text = "Щ";
+            this.char27.UseVisualStyleBackColor = true;
+            // 
+            // char28
+            // 
+            this.char28.Location = new System.Drawing.Point(541, 280);
+            this.char28.Name = "char28";
+            this.char28.Size = new System.Drawing.Size(40, 40);
+            this.char28.TabIndex = 1;
+            this.char28.Text = "Ъ";
+            this.char28.UseVisualStyleBackColor = true;
+            // 
+            // char29
+            // 
+            this.char29.Location = new System.Drawing.Point(583, 280);
+            this.char29.Name = "char29";
+            this.char29.Size = new System.Drawing.Size(40, 40);
+            this.char29.TabIndex = 1;
+            this.char29.Text = "Ы";
+            this.char29.UseVisualStyleBackColor = true;
+            // 
+            // char30
+            // 
+            this.char30.Location = new System.Drawing.Point(625, 280);
+            this.char30.Name = "char30";
+            this.char30.Size = new System.Drawing.Size(40, 40);
+            this.char30.TabIndex = 1;
+            this.char30.Text = "Ь";
+            this.char30.UseVisualStyleBackColor = true;
+            // 
+            // char31
+            // 
+            this.char31.Location = new System.Drawing.Point(667, 280);
+            this.char31.Name = "char31";
+            this.char31.Size = new System.Drawing.Size(40, 40);
+            this.char31.TabIndex = 1;
+            this.char31.Text = "Э";
+            this.char31.UseVisualStyleBackColor = true;
+            // 
+            // char32
+            // 
+            this.char32.Location = new System.Drawing.Point(709, 280);
+            this.char32.Name = "char32";
+            this.char32.Size = new System.Drawing.Size(40, 40);
+            this.char32.TabIndex = 1;
+            this.char32.Text = "Ю";
+            this.char32.UseVisualStyleBackColor = true;
+            // 
+            // char33
+            // 
+            this.char33.Location = new System.Drawing.Point(751, 280);
+            this.char33.Name = "char33";
+            this.char33.Size = new System.Drawing.Size(40, 40);
+            this.char33.TabIndex = 1;
+            this.char33.Text = "Я";
+            this.char33.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 543);
+            this.Controls.Add(this.char33);
+            this.Controls.Add(this.char32);
+            this.Controls.Add(this.char24);
+            this.Controls.Add(this.char16);
+            this.Controls.Add(this.char8);
+            this.Controls.Add(this.char31);
+            this.Controls.Add(this.char23);
+            this.Controls.Add(this.char15);
+            this.Controls.Add(this.char7);
+            this.Controls.Add(this.char30);
+            this.Controls.Add(this.char22);
+            this.Controls.Add(this.char14);
+            this.Controls.Add(this.char6);
+            this.Controls.Add(this.char29);
+            this.Controls.Add(this.char21);
+            this.Controls.Add(this.char13);
+            this.Controls.Add(this.char5);
+            this.Controls.Add(this.char28);
+            this.Controls.Add(this.char20);
+            this.Controls.Add(this.char27);
+            this.Controls.Add(this.char12);
+            this.Controls.Add(this.char19);
+            this.Controls.Add(this.char4);
+            this.Controls.Add(this.char26);
+            this.Controls.Add(this.char11);
+            this.Controls.Add(this.char18);
+            this.Controls.Add(this.char25);
+            this.Controls.Add(this.char3);
+            this.Controls.Add(this.char17);
+            this.Controls.Add(this.char10);
+            this.Controls.Add(this.char9);
+            this.Controls.Add(this.char2);
+            this.Controls.Add(this.char1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "MainForm";
+            this.Text = "Игра \"Виселица\"";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button char1;
+        private System.Windows.Forms.Button char2;
+        private System.Windows.Forms.Button char3;
+        private System.Windows.Forms.Button char4;
+        private System.Windows.Forms.Button char5;
+        private System.Windows.Forms.Button char6;
+        private System.Windows.Forms.Button char7;
+        private System.Windows.Forms.Button char8;
+        private System.Windows.Forms.Button char9;
+        private System.Windows.Forms.Button char10;
+        private System.Windows.Forms.Button char11;
+        private System.Windows.Forms.Button char12;
+        private System.Windows.Forms.Button char13;
+        private System.Windows.Forms.Button char14;
+        private System.Windows.Forms.Button char15;
+        private System.Windows.Forms.Button char16;
+        private System.Windows.Forms.Button char17;
+        private System.Windows.Forms.Button char18;
+        private System.Windows.Forms.Button char19;
+        private System.Windows.Forms.Button char20;
+        private System.Windows.Forms.Button char21;
+        private System.Windows.Forms.Button char22;
+        private System.Windows.Forms.Button char23;
+        private System.Windows.Forms.Button char24;
+        private System.Windows.Forms.Button char25;
+        private System.Windows.Forms.Button char26;
+        private System.Windows.Forms.Button char27;
+        private System.Windows.Forms.Button char28;
+        private System.Windows.Forms.Button char29;
+        private System.Windows.Forms.Button char30;
+        private System.Windows.Forms.Button char31;
+        private System.Windows.Forms.Button char32;
+        private System.Windows.Forms.Button char33;
+    }
+}
+
---
+++ Gallows/Gallows/Menu.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Gallows
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startGame = new System.Windows.Forms.Button();
+            this.leaderboard = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.reference = new System.Windows.Forms.Button();
+            this.addWord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startGame
+            // 
+            this.startGame.Location = new System.Drawing.Point(117, 39);
+            this.startGame.Name = "startGame";
+            this.startGame.Size = new System.Drawing.Size(160, 57);
+            this.startGame.TabIndex = 0;
+            this.startGame.Text = "Играть";
+            this.startGame.UseVisualStyleBackColor = true;
+            // 
+            // leaderboard
+            // 
+            this.leaderboard.Location = new System.Drawing.Point(117, 209);
+            this.leaderboard.Name = "leaderboard";
+            this.leaderboard.Size = new System.Drawing.Size(160, 57);
+            this.leaderboard.TabIndex = 0;
+            this.leaderboard.Text = "Список лидеров";
+            this.leaderboard.UseVisualStyleBackColor = true;
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(117, 379);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(160, 57);
+            this.exit.TabIndex = 0;
+            this.exit.Text = "Выход";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // reference
+            // 
+            this.reference.Location = new System.Drawing.Point(117, 294);
+            this.reference.Name = "reference";
+            this.reference.Size = new System.Drawing.Size(160, 57);
+            this.reference.TabIndex = 0;
+            this.reference.Text = "Справка";
+            this.reference.UseVisualStyleBackColor = true;
+            // 
+            // addWord
+            // 
+            this.addWord.Location = new System.Drawing.Point(117, 124);
+            this.addWord.Name = "addWord";
+            this.addWord.Size = new System.Drawing.Size(160, 57);
+            this.addWord.TabIndex = 0;
+            this.addWord.Text = "Добавить слова";
+            this.addWord.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 485);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.reference);
+            this.Controls.Add(this.addWord);
+            this.Controls.Add(this.leaderboard);
+            this.Controls.Add(this.startGame);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startGame;
+        private System.Windows.Forms.Button leaderboard;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button reference;
+        private System.Windows.Forms.Button addWord;
+    }
+}
